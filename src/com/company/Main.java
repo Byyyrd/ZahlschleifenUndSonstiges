89dc069d843403655d26eeb056cd811499756606
Byyyrd <105438850+Byyyrd@users.noreply.github.com>
2023-04-18 20:23:40 +0200

    [5.01] Kontrollstrukturen II - Zählschleifen Nr.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-        printSomeNumbers();
 
     }
 
@@ -11,5 +9,35 @@ public class Main {
         for (int i = 6; i < 24;i++){
             System.out.println(i);
         }
+    }
+    public static void  printNumbers(int start,int end){
+        for (int i = start; i < end+1;i++){
+            System.out.println(i);
+        }
+    }
+    public static int sumUp(int start,int end){
+        int result = 0;
+        for (int i = start; i < end+1;i++){
+            result += i;
+        }
+        return result;
+    }
+    public static int powerOfTwo(int p){
+        if (p >= 0){
+            int result = 1;
+            for (int i = 0; i < p;i++){
+                result *= 2;
+            }
+            return result;
+        }else {
+            throw new IllegalArgumentException("Number must be greater than or equal to zero");
+        }
+    }
+    static int sumUpPowerOfTwo(int n){
+        int result = 0;
+        for (int i = 0;i<n+1;i++){
+            result += powerOfTwo(i);
+        }
+        return result;
     }
 }
