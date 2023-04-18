5bd27dd5d7e1666ad153ead801c0d7f36697f68d
kolhjuli <kolhjuli@kkg-do.eu>
2023-04-18 10:43:41 +0200

    kahfkiiaghfki

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
-            System.out.println("Wert von i: " + i);
-            if(i == 3){
-                System.out.println("Holdriho!");
-            }
-        }
+        printSomeNumbers();
 
+    }
+
+    public static void  printSomeNumbers(){
+        for (int i = 6; i < 24;i++){
+            System.out.println(i);
+        }
     }
 }
