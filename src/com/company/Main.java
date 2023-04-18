b10b3c37b3597220e93c683613ea1b66b57f032b
kolhjuli <kolhjuli@kkg-do.eu>
2023-04-18 10:40:34 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            System.out.println("Wert von i: " + i);
+            if(i == 3){
+                System.out.println("Holdriho!");
+            }
+        }
+
+    }
+}
